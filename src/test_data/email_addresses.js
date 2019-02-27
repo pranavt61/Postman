fde12248ac95db89291b341b8a12983133162386
Pranav <pranav@dhcp-10-15-186-36.wireless.pacific.edu>
2019-02-27 14:45:12 -0800

    Add test email addresses

---
+++ src/test_data/email_addresses.js
@@ -0,0 +1,14 @@
+export default const email_addresses = [
+  {
+    name: 'Pranav Thirunavukkararsu',
+    address: 'p_thiru@postman.com'
+  },
+  {
+    name: 'Seth Kith',
+    address: 's_kith@postman.com'
+  },
+  {
+    name: 'Antonio Tran',
+    address: 'a_tran@postman.com'
+  }
+];
